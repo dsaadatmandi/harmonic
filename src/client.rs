affef40237d3f9ca926d6f4a6eddd451df7d1a36
David Milad Saadatmandi <davidmilad@gmail.com>
2025-10-20 01:01:22 +0200

    improvements

--- src/client.rs
+++ src/client.rs
@@ -1,19 +1,25 @@
-
+use harmonic::ClientSyncState;
 use harmonic::harmonic_client::HarmonicClient;
-use harmonic::{HarmonizePushRequest};
+use log::info;
 use tokio_stream::StreamExt;
+use uuid::Uuid;
+
+use crate::common::ChangeType;
+use crate::harmonic::ServerSyncStateResponse;
 
 pub mod common;
-
 
 pub mod harmonic {
     tonic::include_proto!("harmonic");
 }
 
+const ADDR: &str = "http://[::1]:42069";
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let sync_uuid = Uuid::new_v4();
 
-    let config= common::load_config();
+    let config = common::load_config();
 
     let last_state = common::load_state(&config);
 
@@ -24,34 +30,45 @@ fn main() {
 
     let now_state = common::generate_state(config.sync_path);
 
-    let diffs = common::compare_states(last_state, now_state);
+    let diffs = common::compare_states(&last_state, &now_state);
 
-    
-
-}
-
-
-#[tokio::main]
-async fn send_state_to_client() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = HarmonicClient::connect("http://[::1]:42069").await?;
-
-    let request = tonic::Request::new(HarmonizePushRequest {
-        request: "pls".to_string(),
-    });
-
-    let mut response = client.harmonize_server_stream_update(request).await?.into_inner();
-
-    while let Some(r) = response.next().await {
-        match r {
-            Ok(reply) => {
-                println!("received: {:?}", reply)
-            }
-            Err(e) => {
-                println!("{:?}", e)
-            }
-        }
+    if diffs.len() == 0 {
+        info!("No updates to push");
+        ()
     }
 
+    let response = send_state_to_server(&sync_uuid, last_state.last_sync_timestamp_micros, diffs)
+        .await
+        .expect("Error awaiting response from server to sync intiation.");
+}
 
-    Ok(())
+async fn send_state_to_server(
+    sync_uuid: &Uuid,
+    last_sync_timestamp: i64,
+    diffs: Vec<common::Diff>,
+) -> Result<ServerSyncStateResponse, Box<dyn std::error::Error>> {
+    let mut client = HarmonicClient::connect(ADDR).await?;
+
+    let request = tonic::Request::new(ClientSyncState {
+        sync_uuid: sync_uuid.to_string(),
+        timestamp_last_sync_micro: last_sync_timestamp,
+        status_list: diffs.into_iter().map(Into::into).collect(),
+    });
+
+    let response = client
+        .harmonize_client_initiate_sync(request)
+        .await?
+        .into_inner();
+
+    Ok(response)
+    // while let Some(r) = response.next().await {
+    //     match r {
+    //         Ok(reply) => {
+    //             println!("received: {:?}", reply)
+    //         }
+    //         Err(e) => {
+    //             println!("{:?}", e)
+    //         }
+    //     }
+    // }
 }
--- src/common.rs
+++ src/common.rs
@@ -1,36 +1,26 @@
 use chrono::prelude::Utc;
-use std::{collections::{BTreeMap, BTreeSet}, fs::{self}, path::{Path,  PathBuf}, time::UNIX_EPOCH};
-use serde::{Serialize, Deserialize};
+use log::info;
 use serde;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fs::{self},
+    path::{Path, PathBuf},
+    time::UNIX_EPOCH,
+};
 use walkdir::WalkDir;
-use log::{info};
 
-
-// pub struct HealthStatus {
-//     time: i64,
-//     state: HealthStatus,
-// }
-
-// impl HealthStatus {
-//     pub fn new(state: Status) -> Self {
-//         HealthStatus {
-//             time: Utc::now().timestamp_micros(),
-//             state,
-//         }
-//     }
-// }
-
+use crate::harmonic::{FileStatus, FileType, UpdateStrategy};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     uuid: uuid::Uuid,
     pub sync_path: PathBuf,
-
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct SyncState {
-    last_sync_timestamp_micros: i64,
+    pub last_sync_timestamp_micros: i64,
     tree: BTreeMap<PathBuf, FileMetadata>,
 }
 
@@ -42,7 +32,9 @@ struct FileMetadata {
 
 pub struct Diff {
     path: PathBuf,
-    change: ChangeType,
+    pub change: ChangeType,
+    hash: [u8; 16],
+    modified_ts: i64,
 }
 
 pub enum ChangeType {
@@ -51,34 +43,56 @@ pub enum ChangeType {
     Modified,
 }
 
+impl From<Diff> for FileStatus {
+    fn from(diff: Diff) -> Self {
+        FileStatus {
+            path: diff
+                .path
+                .to_str()
+                .expect("Issue converting strange chars.")
+                .to_string(),
+            timestamp_micro: diff.modified_ts,
+            file_type: FileType::Other.into(),
+            hash: diff.hash.to_vec(),
+        }
+    }
+}
+
+// impl From<ChangeType> for UpdateStrategy {
+//     fn from(change: ChangeType) -> Self {
+//         match change {
+//             ChangeType::Added => UpdateStrate::
+//             ChangeType::Removed => UpdateStrategy::ClientPull
+//             ChangeType::Modified => U
+//         }
+//     }
+// }
+
 impl FileMetadata {
     fn new<P: AsRef<Path>>(path: P) -> FileMetadata {
         let path = path.as_ref();
-        let file = fs::read(&path)
-        .expect("Failed to open file.");
+        let file = fs::read(&path).expect("Failed to open file.");
         let hash: [u8; 16] = md5::compute(&file).into();
         let modified_systime = fs::metadata(&path)
-        .expect("Unable to read metadata for file")
-        .modified()
-        .expect("Unable to read modified time for file");
+            .expect("Unable to read metadata for file")
+            .modified()
+            .expect("Unable to read modified time for file");
 
         let modified_ts = modified_systime
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_micros() as i64;
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_micros() as i64;
 
-        Self {hash, modified_ts}
+        Self { hash, modified_ts }
     }
 }
 
 fn config_dir_path() -> PathBuf {
-    let mut path = dirs::config_dir()
-    .expect("No path could be created for config dir");
+    let mut path = dirs::config_dir().expect("No path could be created for config dir");
     path.push("harmonic");
 
     path
 }
-
 
 fn config_file_path() -> PathBuf {
     let mut path = config_dir_path();
@@ -88,76 +102,97 @@ fn config_file_path() -> PathBuf {
 }
 
 fn save_config(config: Config) {
-    let config_toml = toml::to_string(&config)
-    .expect("Unable to serialize config struct to toml format.");
+    let config_toml =
+        toml::to_string(&config).expect("Unable to serialize config struct to toml format.");
 
     fs::write(config_file_path(), config_toml)
-    .expect("Unable to write serialized config struct to file.");
+        .expect("Unable to write serialized config struct to file.");
 }
 
 pub fn load_config() -> Config {
-    let config_toml = fs::read_to_string(config_file_path())
-    .expect("Unable to read file");
+    let config_toml = fs::read_to_string(config_file_path()).expect("Unable to read file");
 
-    toml::from_str(&config_toml)
-    .expect("Unable to parse string to toml")
-
+    toml::from_str(&config_toml).expect("Unable to parse string to toml")
 }
 
 pub fn save_state(state: SyncState, config: &Config) {
-    let state_json = serde_json::to_string(&state)
-    .expect("Unable to serialise state to json format.");
+    let state_json =
+        serde_json::to_string(&state).expect("Unable to serialise state to json format.");
 
     fs::write(&config.sync_path, state_json)
-    .expect("Unable to write serialized Sync State struct to file.");
+        .expect("Unable to write serialized Sync State struct to file.");
 }
 
 pub fn load_state(config: &Config) -> SyncState {
-    let state_json = fs::read_to_string(&config.sync_path)
-    .expect("Unable to read file");
+    let state_json = fs::read_to_string(&config.sync_path).expect("Unable to read file");
 
-    serde_json::from_str(&state_json)
-    .expect("Unable to parse string to toml")
+    serde_json::from_str(&state_json).expect("Unable to parse string to toml")
 }
 
 pub fn generate_state(root_path: PathBuf) -> SyncState {
-
     let mut file_tree: BTreeMap<PathBuf, FileMetadata> = BTreeMap::new();
 
     // TODO: log
     for file in WalkDir::new(root_path)
-    .into_iter()
-    .filter_map(|e| e.ok())
-    .filter(|e| e.metadata().unwrap().is_file()) {
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.metadata().unwrap().is_file())
+    {
         let metadata = FileMetadata::new(file.path());
         file_tree.insert(file.into_path(), metadata);
     }
 
-    SyncState { last_sync_timestamp_micros: Utc::now().timestamp_micros(), tree: file_tree }
-
+    SyncState {
+        last_sync_timestamp_micros: Utc::now().timestamp_micros(),
+        tree: file_tree,
+    }
 }
 
-pub fn compare_states(last_state: SyncState, now_state: SyncState) -> Vec<Diff> {
+pub fn compare_states(before_state: &SyncState, now_state: &SyncState) -> Vec<Diff> {
     info!("Computing difference between current state with previous sync state");
 
     let mut diffs = Vec::new();
 
-    let all_paths: BTreeSet<&PathBuf> = last_state.tree.keys().chain(now_state.tree.keys()).collect();
+    let all_paths: BTreeSet<&PathBuf> = before_state
+        .tree
+        .keys()
+        .chain(now_state.tree.keys())
+        .collect();
 
     for path in all_paths {
-        let before = last_state.tree.get(path);
+        let before = before_state.tree.get(path);
         let now = now_state.tree.get(path);
 
         match (now, before) {
-            (Some(now_meta), Some(before_meta)) if now_meta.hash != before_meta.hash 
-            => {diffs.push(Diff{path: path.to_owned(), change: ChangeType::Modified})}
-            (Some(_), None) => {diffs.push(Diff{path: path.to_owned(), change: ChangeType::Added})}
-            (None, Some(_)) => {diffs.push(Diff{path:path.to_owned(), change: ChangeType::Removed})}
+            (Some(now_meta), Some(before_meta)) if now_meta.hash != before_meta.hash => {
+                let (hs, mts) = if now_meta.modified_ts > before_meta.modified_ts {
+                    (now_meta.hash, now_meta.modified_ts)
+                } else {
+                    (before_meta.hash, before_meta.modified_ts)
+                };
+                diffs.push(Diff {
+                    path: path.to_owned(),
+                    change: ChangeType::Modified,
+                    hash: hs,
+                    modified_ts: mts,
+                })
+            }
+            (Some(meta), None) => diffs.push(Diff {
+                path: path.to_owned(),
+                change: ChangeType::Added,
+                hash: meta.hash,
+                modified_ts: meta.modified_ts,
+            }),
+            (None, Some(meta)) => diffs.push(Diff {
+                path: path.to_owned(),
+                change: ChangeType::Removed,
+                hash: meta.hash,
+                modified_ts: meta.modified_ts,
+            }),
             (Some(_), Some(_)) => {}
-            (None, None) => unreachable!()
+            (None, None) => unreachable!(),
         }
-    };
+    }
 
     diffs
-    
 }
